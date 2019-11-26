ff2c31cb7dea0538fc028a369e881664f1e30274
devantheryl <devantheryl@windowslive.com>
2019-11-26 09:20:49 +0100

    creating cmake list
    first program writed in hello.cpp + cmake list created

---
+++ hello.cpp
@@ -0,0 +1,11 @@
+#include <cstdio>
+#include <unistd.h>
+
+int main(int argc, char** argv){
+	puts("hello PI");
+	sleep(1);
+	puts("this is my first program on a raspberry pi...");
+	puts("<press any key>");
+	getchar();
+	
+}
